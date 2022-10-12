566c41e5f1fff82e6c53b3eb0d482c5c57a9048c
ZodiaX1013 <jaydeepgamit36@gmail.com>
2022-10-12 11:29:45 +0530

    commit

--- static/js/custom.js
+++ static/js/custom.js
@@ -714,3 +714,47 @@ function Export2Word(element, filename = 'paysheet'){
   
   document.body.removeChild(downloadLink);
 }
+
+
+
+// Export To Doc With Image
+
+function ExportToDoc(filename = ''){
+  var HtmlHead = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+
+  var EndHtml = "</body></html>";
+
+  //complete html
+  var html = HtmlHead +document.getElementById("exportContent").innerHTML+EndHtml;
+
+  //specify the type
+  var blob = new Blob(['\ufeff', html], {
+      type: 'application/msword'
+  });
+  
+  // Specify link url
+  var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+  
+  // Specify file name
+  filename = filename?filename+'.doc':'document.doc';
+  
+  // Create download link element
+  var downloadLink = document.createElement("a");
+
+  document.body.appendChild(downloadLink);
+  
+  if(navigator.msSaveOrOpenBlob ){
+      navigator.msSaveOrOpenBlob(blob, filename);
+  }else{
+      // Create a link to the file
+      downloadLink.href = url;
+      
+      // Setting the file name
+      downloadLink.download = filename;
+      
+      //triggering the function
+      downloadLink.click();
+  }
+  
+  document.body.removeChild(downloadLink);
+}
